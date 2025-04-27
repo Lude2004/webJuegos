94aab83202d94e83a236dc31c14f45426b0e6eed
Lude2004 <alexis.ludena@unl.edu.ec>
2025-04-26 19:15:28 -0500

    actualizacion f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: 
+  [
+    RouterOutlet,
+    MatCardModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'webJuegos';
